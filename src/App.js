9c604653f1ba8c3cd2d3699e283ad1fd3992143c
Kajal Saini <sizukabts91@gmail.com>
2025-08-25 16:23:18 +0530

    Add scrollbutton

--- src/App.js
+++ src/App.js
@@ -2,16 +2,23 @@ import About from './components/About/About';
 import Header from './components/Header/Header';
 import NavigationBar from './components/NavigationBar';
 import { React, useState } from 'react';
+import { Fragment } from "react";
+import Scrollbutton from "./components/Scrollbutton";
+
+
 function App() {
     const [darkMode, setDarkMode] = useState(false);
 
   return (
+    <Fragment>
     <div className="App">
       <NavigationBar darkMode={darkMode}
       setDarkMode={setDarkMode} />
       <Header darkMode={darkMode}/>
       <About darkMode={darkMode}/>
+      <Scrollbutton/>
     </div>
+    </Fragment>
   );
 }
 
