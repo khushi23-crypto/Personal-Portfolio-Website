0fb6e2c15e14522652504c7499614c5fd5b54840
Kajal Saini <sizukabts91@gmail.com>
2025-08-26 12:34:53 +0530

    Create Contact section

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 
-function Contact() {
+function Contact({darkMode}) {
   return (
-    <div>C</div>
+    <div className={`contact ${darkMode ? 'contact-light' : 'contact-dark'}`}>
+            <h1 className="contact-heading">Let's Connect with me</h1>
+            <p className='c-para'> Explore my latest work and see how I bring ideas to life through code.</p>
+            </div>
   )
 }
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from 'react'
 import Photo from '../../images/my photo1.jpg'
 import { MdOutlineArrowOutward } from "react-icons/md";
 import { motion, AnimatePresence } from "framer-motion";
+import { Link } from "react-router-dom";
 
 function Header({ darkMode }) {
 
@@ -51,16 +52,52 @@ function Header({ darkMode }) {
             <p className='myself'>I'm a passionate Web Designer and Developer with a strong foundation in creating modern, responsive, and user-friendly websites. Currently, I’m pursuing my Bachelor of Technology in Information Technology at Rajkiya Engineering College, Azamgarh (2022-2026).</p>
           </div>
           <div className="flex">
-            <a href='#resume' className={`openresume ${darkMode ? 'openresume-light' : 'openresume-dark'}`}>
+            <Link to='/resume' className={`openresume ${darkMode ? 'openresume-light' : 'openresume-dark'}`}>
               Open Resume&nbsp;<MdOutlineArrowOutward />
-            </a>
-            <div className={`nav-connect ${darkMode ? 'nav-connect-light' : 'nav-connect-dark'}`}>
+            </Link>
+            <Link to="/contact" className={`nav-connect ${darkMode ? 'nav-connect-light' : 'nav-connect-dark'}`}>
               Connect With Me
-            </div>
+            </Link>
           </div>
         </div>
       </header >
-      
+      <div className={`experience ${darkMode ? 'experience-light' : 'experience-dark'}`}>
+        <h1 className='feedback'><span className='color'>Achievements </span>in the tech industries</h1>
+        <div className='card-container'>
+          <div className='card'>
+            <div className="card-body">
+              <h3 className="card-title">⭐⭐⭐⭐⭐</h3>
+              <p className="card-text">❛❛Kajal Saini shows excellent growth in frontend development, demonstrating strong skills in React, CSS, and UI design with consistent improvement, creativity, and dedication to learning modern practices.❞</p>
+              <h3 className='company'>~CODEXINTERN</h3>
+            </div>
+          </div>
+
+          <div className="card">
+            <div className="card-body">
+              <h3 className="card-title">⭐⭐⭐⭐</h3>
+              <p className="card-text">❛❛Kajal Saini performs excellently in full-stack development, managing both frontend and backend efficiently, with great problem-solving skills, database handling, API integration, and consistent focus on scalable, maintainable applications.❞</p>
+              <h3 className='company'>~GNCPIL</h3>
+            </div>
+          </div>
+
+          <div className="card">
+            <div className="card-body">
+              <h3 className="card-title">⭐⭐⭐⭐</h3>
+              <p className="card-text">❛❛Kajal Saini exhibits strong potential in machine learning, showcasing analytical skills, model-building expertise, data handling capabilities, and continuous learning attitude to develop intelligent, efficient, and innovative solutions effectively.❞</p>
+              <h3 className='company'>~ProdigyInfo Tech</h3>
+            </div>
+          </div>
+
+          <div className="card">
+            <div className="card-body">
+              <h3 className="card-title">⭐⭐⭐⭐⭐​​</h3>
+              <p className="card-text">❛❛Kajal Saini excels in web development, demonstrating expertise in modern frameworks, responsive design, clean coding practices, optimization techniques, and a proactive approach to delivering user-friendly and high-performing websites.❞</p>
+              <h3 className='company'>~Codsoft</h3>
+            </div>
+          </div>
+        </div>
+
+      </div>
     </>
   )
 }
