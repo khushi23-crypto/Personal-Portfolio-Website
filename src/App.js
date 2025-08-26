faed11ef4fb5a6c59396913584d73f8c37da9407
Kajal Saini <sizukabts91@gmail.com>
2025-08-26 12:13:17 +0530

    Create Project section

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import NavigationBar from './components/NavigationBar';
 import { React, useState } from 'react';
 import Scrollbutton from "./components/Scrollbutton";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-
+import Skills from './components/Skills/Skills';
+import Project from './components/Project/Project'
+import Contact from './components/Contact/Contact';
 function App() {
     const [darkMode, setDarkMode] = useState(false);
 
@@ -18,7 +19,9 @@ function App() {
       <Routes>
       <Route path="/home" element={<Header darkMode={darkMode}/>}/>
       <Route path="/about" element={<About darkMode={darkMode}/>}/>
-      
+      <Route path="/skills" element={<Skills darkMode={darkMode}/>}/>
+      <Route path="/projects" element={<Project darkMode={darkMode}/>}/>
+      <Route path="/contact" element={<Contact darkMode={darkMode}/>}/>
       </Routes>
       <Scrollbutton/>
     </Router>
